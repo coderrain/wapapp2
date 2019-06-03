dbea854b624be482679af22cab72c33e25005d08
lijinpeng521 <1602788223@qq.com>
2019-06-03 11:58:20 +0800

    提交test文件

--- src/container/Home.vue
+++ src/container/Home.vue
@@ -1,8 +1,11 @@
 <template>
     <div>
         <h-header></h-header>
-       <!-- <search></search>
-        <swiper></swiper>
+		<div class="search">
+			<input type="" name="">
+		</div>
+        	
+        <!-- <swiper></swiper>
         <menu></menu>
         <div></div>
         <list></list>-->
@@ -20,5 +23,11 @@
 </script>
 
 <style scoped>
-
+.search{
+	height:0.88rem;
+	background:#51dfe0;
+	input{
+	
+	}
+}
 </style>
